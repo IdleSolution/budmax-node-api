db76f4998c84695ad75736d352c041663a11d79a
IdleSolution <idlesoluszyn@gmail.com>
2024-03-16 22:39:10 +0100

    add cors

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,14 @@ import * as bodyParser from 'body-parser';
 import { MainRouter } from './routes';
 import swaggerUi from 'swagger-ui-express'
 import swaggerDocument from './swagger-output.json';
+import cors from 'cors';
 
 import './database';
 
 const app: express.Application = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
     console.log('hello');
